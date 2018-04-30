3015836e6037dec87fa61c1366306bc0e94042a2
andreas.kaeberlein <andreas.kaeberlein@web.de>
2018-04-30 11:04:31 +0200

    espec_corp_sh_641.py -> prepared header in file

---
+++ src/drv/espec_corp_sh_641.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Provides basic functions to interface the climate chamber
+ * ESPEC CORP. SH-641
+"""
+
+__author__ 		= "Andreas Kaeberlein"
+__copyright__ 	= "Copyright 2018, Arbitrary Temperature Waveform Generator"
+
+__license__ 	= "LGPLv3"
+__version__ 	= "0.1.0"
+__maintainer__ 	= "Andreas Kaeberlein"
+__email__ 		= "andreas.kaeberlein@web.de"
+__status__ 		= "Development"
+
