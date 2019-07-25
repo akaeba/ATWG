cbff00ecfd23541c1e2d469605b82e98f5ad43bc
Andreas Kaeberlein <andreas.kaeberlein.ext@siemens.com>
2019-07-25 16:27:51 +0200

    file holder

---
+++ src/ATWG.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Arbitrary temperature waveform generator
+"""
+
+__FILE__        = "ATWG.py"
+__author__ 		= "Andreas Kaeberlein"
+__copyright__ 	= "Copyright 2019, Arbitrary Temperature Waveform Generator"
+__credits__     = ["AKAE"]
+
+__license__ 	= "LGPLv3"
+__version__ 	= "0.1.0"
+__maintainer__ 	= "Andreas Kaeberlein"
+__email__ 		= "andreas.kaeberlein@web.de"
+__status__ 		= "Development"
+
+
+
+#------------------------------------------------------------------------------
+import espec_corp_sh_641_drv    # driver for climate chamber
+#------------------------------------------------------------------------------
+
+
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    pass
+#------------------------------------------------------------------------------
+   
