9167106bbb5762b33d3589b5f7174072dd81b7cd
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2019-07-29 22:51:59 +0200

    add clima_set and clima_meas method, begin of start method

--- src/ATWG.py
+++ src/ATWG.py
@@ -20,7 +20,7 @@ __status__       = "Development"
 import sys                      # CLI arguments
 import argparse                 # argument parser
 import time                     # time
-#import espec_corp_sh_641_drv    # driver for climate chamber
+import espec_corp_sh_641_drv    # driver for climate chamber
 #------------------------------------------------------------------------------
 
 
@@ -31,10 +31,21 @@ class ATWG:
         """
         Initialization
         """
+        # features
         self.supported_chamber = ["ESPEC_SH641",]      # supported temperature chambers
-        self.selected_chamber = float("nan")           # selected chambers
         self.supported_waveforms = ["const", "sine"]   # implemented waveform algorithms
+        # used config
+        self.selected_chamber = float("nan")           # selected chambers
         self.selected_waveform = float("nan")          # choosen waveform
+        self.interface = ""                            # communication interface
+        # temperture setting
+        self.tset = float("nan")        # setted temperature
+        self.tmeas = float("nan")       # measured temperature
+        self.hset = float("nan")        # setted humidity
+        self.hmeas = float("nan")       # measured humidity
+        # supported chamber classes
+        self.espesShSu = espec_corp_sh_641_drv.especShSu()    # create class without constructor call
+        
 
     #*****************************
     
@@ -92,8 +103,97 @@ class ATWG:
         
         return True
     #*****************************
-        
     
+    
+    #*****************************
+    def chamber_start(self):
+        """
+        Starts Chamber 
+            1) opens interface
+            2) initialize chamber
+            3) start temperature control
+    
+        Return:
+            True:
+            False:
+        """
+        # open phy interface to chamber
+        if ( self.supported_chamber[self.selected_chamber] == "ESPEC_SH641"):
+            self.espesShSu.config_com(port=self.interface)  # call constructor with port
+            self.espesShSu.open()                           # open interface to chamber
+        else:
+            print("Error: Unsupported Chamber '" + self.supported_chamber[self.selected_chamber] + "'")
+            return False
+        # graceful end
+        return True
+    #*****************************
+    
+    
+    #*****************************
+    def chamber_clima_meas(self):
+        """
+        measures chambers current values and
+        stores result in self.tmeas and self.hmeas
+        
+        Return:
+            True:   all good
+            False:  something went wrong
+        """
+        # dispatch to chamber driver
+        if ( self.supported_chamber[self.selected_chamber] == "ESPEC_SH641" ):
+            # read value
+            temp = self.espesShSu.get_temp()
+            # succesful?
+            if ( False == temp ):
+                print("Error: Read measured temperature from chamber '" + self.supported_chamber[self.selected_chamber] + "'")
+                return False
+            # assign to internal
+            self.tmeas = temp["measured"]
+            # read value
+            humi = self.espesShSu.get_humidity()
+            # succesful?
+            if ( False == humi ):
+                print("Error: Read measured humidity from chamber '" + self.supported_chamber[self.selected_chamber] + "'")
+                return False
+            # assign
+            self.hmeas = humi["measured"]
+        else:
+            print("Error: Unsupported Chamber")
+            return False
+        # graceful end
+        return True
+    #*****************************
+    
+    
+    #*****************************
+    def chamber_clima_set(self):
+        """
+        Updates chambers climate settings - tempetature and humidity
+        
+        Return:
+            True:   all good
+            False:  something went wrong
+        """
+        # dispatch to chamber driver
+        if ( self.supported_chamber[self.selected_chamber] == "ESPEC_SH641" ): 
+            # set temperature value
+            if ( float("nan") != self.tset ):
+                if ( False == self.espesShSu.set_temp(self.tset) ):
+                    print("Error set new temperature value to chamber '" + self.supported_chamber[self.selected_chamber] + "'")
+                    return False
+            # set humidity
+            if ( float("nan") != self.hset ):
+                print("Error huminity set from chamber '" + self.supported_chamber[self.selected_chamber] + "' not implemented")
+                return False
+        else:
+            print("Error: Unsupported Chamber '" + self.supported_chamber[self.selected_chamber] + "'")
+            return False
+        # graceful end
+        return True        
+    #*****************************
+    
+    
+
     #***************************** 
     def main(self):
         """
