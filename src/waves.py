61483d124594078a8dc6a96455688949a72ceace
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2019-08-25 12:30:05 +0200

    separated calculation function from init function

--- src/waves.py
+++ src/waves.py
@@ -34,28 +34,15 @@ class waves:
     def __init__(self):
         """
         @note:          intializises class
-
-        @return:        boolean
         """
-        self.sample_sec = 1     # sample time is 1sec
-        self.period_sec = 3600  # periode in sec
-        self.iterator = 0       # waveform iterator
-    #*****************************
-    
-    
-    #*****************************
-    def configure(self, sample=1, period=3600):
-        """
-        @note:          configures class
-        
-        @param sample:  Sample/Update time of waveform  in seconds
-        @param period:  Period time of waveform in seconds
-        
-        @return:        boolean
-        """
-        self.sample_sec = sample
-        self.period_sec = period
-        return True                 # all done
+        self.sample_sec = 1                     # sample time is 1sec
+        self.period_sec = 3600                  # periode in sec
+        self.iterator = 0                       # waveform iterator
+        self.verbose = 0                        # suppress messages
+        self.trapezoidDict = {}                 # storage element for trapezoid function
+        self.trapezoidDict['isInit'] = False    # set uninitialized
+        self.sineDict = {}                      # 
+        self.sineDict['isInit'] = False         #
     #*****************************
     
     
@@ -74,29 +61,55 @@ class waves:
         except ZeroDivisionError:
             return float("nan")
     #*****************************
+    
+    
+    #*****************************
+    def clear_init(self):
+        """
+        @note:      clear init flags
+        
+        @return:    successful
+        """
+        # clear flags
+        self.trapezoidDict['isInit'] = False
+        self.sineDict['isInit'] = False
+        # graceful end
+        return True
+    #*****************************
 
 
     #*****************************
-    def sine(self, amp=None, ofs=None, init=None, posSlope=True):
+    def sine_init(self, sample=None, period=None, low=None, high=None, init=None, posSlope=True):
         """
-        @note:              calculates sine waveform
+        @note:              initializes sine waveform
         
         @param amp:         amplitude of sine
         @param ofs:         offset of sine
         @param init:        initialializes temperature wave form
         @type init:         None: no init, calc next setting temperature; number: adjust to set temperature; nan: start at zero in waveform
         @param posSlope:    selects positive/negativ slope side of sine, only evaluated in init
-        @type posSlope:     True: sine starts with increasing his values; False: sine start with decreasing his values
+        @type posSlope:     True: sine starts with increasing his values; False: sine start with decreasing his values 
         
-        @return new:        dictionary with new setting
-        @rtype new:         val: new value; grad: gradient of waveform
+        @return:            init succesful
         """
-        # preparation
-        n = round(self.period_sec/self.sample_sec)    # number of steps for full periode
-        new = {}                                            # dictionary with calc result
-        # check for args
-        if ( None == amp or None == ofs ):
-            return False
+        # clear init flags
+        if ( True != self.clear_init() ):
+            if ( 0 != self.verbose ):
+                print("Error: Clear Flags")
+            return False        
+        # check for mandatory args
+        if ( None == sample or None == period or None == low or None == high ):
+            if ( 0 != self.verbose ):
+                print("Error: Missing Argument")
+            return False        
+        # update waveform settings
+        self.sample_sec = sample
+        self.period_sec = period
+        self.period_n = round(self.period_sec/self.sample_sec)      # number of steps for full periode
+        # calc offset amplitude
+        waveform = {}
+        waveform['amp'] = (high - low) / 2
+        waveform['ofs'] = waveform['amp'] + low 
         # init sine?
         if ( None != init ):
             # sine allign to temp desiered?
@@ -104,48 +117,76 @@ class waves:
                 self.iterator = 0
             else:
                 # ensure inside temp range
-                init = min(init, ofs + amp)     # apply upper fence
-                init = max(init, ofs - amp)     # applay lower fence
+                init = min(init, waveform['ofs'] + waveform['amp'])     # apply upper fence
+                init = max(init, waveform['ofs'] - waveform['amp'])     # applay lower fence
                 # init wave iterator
-                self.iterator = (self.period_sec/(2*math.pi*self.sample_sec)) * math.asin((init-ofs)/(amp))
+                self.iterator = (self.period_sec/(2*math.pi*self.sample_sec)) * math.asin((init-waveform['ofs'])/(waveform['amp']))
                 self.iterator = round(self.iterator)
                 # shift to first period
                 if ( self.iterator < 0 ):
-                    self.iterator  += n
-                if ( self.iterator > n-1 ):
-                    self.iterator  -= n
+                    self.iterator  += self.period_n
+                if ( self.iterator > self.period_n-1 ):
+                    self.iterator  -= self.period_n
                 # change slew direction
                 if ( True == posSlope ):
-                    if ( 0.25*n < self.iterator <= 0.5*n ):    # shift to [0*n, 0.25*n]
-                        self.iterator = 0.25*n - self.iterator
-                    elif ( 0.5*n < self.iterator <= 0.75*n ):  # shift to [0.75*n, n]
-                        self.iterator = 0.75*n + (0.75*n-self.iterator)
+                    if ( 0.25*self.period_n < self.iterator <= 0.5*self.period_n ):    # shift to [0*n, 0.25*n]
+                        self.iterator = 0.25*self.period_n - self.iterator
+                    elif ( 0.5*self.period_n < self.iterator <= 0.75*self.period_n ):  # shift to [0.75*n, n]
+                        self.iterator = 0.75*self.period_n + (0.75*self.period_n-self.iterator)
                 else:
-                    if ( 0 <= self.iterator <= 0.25*n):        # shift to [0.25*n, 0.5*n]
-                        self.iterator = 0.25*n + (0.25*n - self.iterator)
-                    elif ( 0.75*n < self.iterator <= n):       # shift to [0.5*n, 0.75*n]
-                        self.iterator = 0.75*n - (self.iterator - 0.75*n)
-                # enw w/o any set temp - it's init part
-                return True
+                    if ( 0 <= self.iterator <= 0.25*self.period_n):        # shift to [0.25*n, 0.5*n]
+                        self.iterator = 0.25*self.period_n + (0.25*self.period_n - self.iterator)
+                    elif ( 0.75*self.period_n < self.iterator <= self.period_n):       # shift to [0.5*n, 0.75*n]
+                        self.iterator = 0.75*self.period_n - (self.iterator - 0.75*self.period_n)
+        else:
+            if ( 0 != self.verbose ):
+                print("Error: Init function with init arg called")
+            return False
+        # store in common storage element
+        self.sineDict['wave'] = waveform        
+        # wave is initialized
+        self.sineDict['isInit'] = True
+        # normal end
+        return True
+    #*****************************
+    
+    
+    #*****************************
+    def sine(self):
+        """
+        @note:              calculates sine waveform
+        
+        @return new:        dictionary with new setting
+        @rtype new:         val: new value; grad: gradient of waveform
+        """
+        # check for init
+        if (True != self.sineDict.get('isInit')):
+            if ( 0 != self.verbose ):
+                print("Error: 'trapezoid' function unintializied, call 'trapezoid_init'")
+            return False
+        # create dict
+        new = {}
         # calculate discrete sine
-        new['val'] = ofs + amp*(math.sin(2*math.pi*((self.iterator)*self.sample_sec/self.period_sec)))
+        new['val'] = self.sineDict.get('wave',{}).get('ofs') + self.sineDict.get('wave',{}).get('amp')*(math.sin(2*math.pi*((self.iterator)*self.sample_sec/self.period_sec)))
         # calc gradient, derived discrete sine
-        new['grad'] = amp*(2*math.pi*self.sample_sec/self.period_sec)*(math.cos(2*math.pi*((self.iterator)*self.sample_sec/self.period_sec)))
+        new['grad'] = self.sineDict.get('wave',{}).get('amp')*(2*math.pi*self.sample_sec/self.period_sec)*(math.cos(2*math.pi*((self.iterator)*self.sample_sec/self.period_sec)))
         # prepare for next calc
         self.iterator += 1
         # jump to sine start
-        if ( self.iterator > n-1 ):
-            self.iterator -= n
+        if ( self.iterator > self.period_n-1 ):
+            self.iterator -= self.period_n
         # graceful end
         return new
     #*****************************
     
     
     #*****************************
-    def trapezoid(self, low=None, high=None, init=None, rise=0, fall=0, dutyMax=float(0.5), posSlope=True):
+    def trapezoid_init(self, sample=None, period=None, low=None, high=None, init=None, rise=0, fall=0, dutyMax=float(0.5), posSlope=True):
         """
-        @note:              calculates trapezoid waveform
+        @note:              initializes trapezoid waveform
         
+        @param sample:      Sample/Update time of waveform  in seconds
+        @param period:      Period time of waveform in seconds
         @param low:         minimal value of trapezoid
         @param high:        maximal value of trapezoid
         @param rise:        rise time in sec from min to max
@@ -156,30 +197,40 @@ class waves:
         @param posSlope:    selects positive/negativ slope side of sine, only evaluated in init
         @type posSlope:     True: sine starts with increasing his values; False: sine start with decreasing his values
         
-        @return new:        dictionary with new setting
-        @rtype new:         val: new value; grad: gradient of waveform
+        @return:            init succesful
         """
+        # clear init flags
+        if ( True != self.clear_init() ):
+            if ( 0 != self.verbose ):
+                print("Error: Clear Flags")
+            return False
         # check for mandatory args
-        if ( None == low or None == high ):
+        if ( None == sample or None == period or None == low or None == high ):
+            if ( 0 != self.verbose ):
+                print("Error: Missing Argument")
             return False
         # check for periode length
         if ( 0 > self.period_sec - rise - fall ):
-            print("Error: Rise/Fall time to large for period length, minimal period length is " + str(rise+fall) +"s")
+            if ( 0 != self.verbose ):
+                print("Error: Rise/Fall time to large for period length, minimal period length is " + str(rise+fall) +"s")
             return False
+        # update waveform settings
+        self.sample_sec = sample
+        self.period_sec = period
+        self.period_n = round(self.period_sec/self.sample_sec)      # number of steps for full periode
         # calc number of cycles
-        n = round(self.period_sec/self.sample_sec)                  # number of steps for full periode
-        step_rise_n = round(rise/self.sample_sec)                   # number of steps for rise
-        step_fall_n = round(fall/self.sample_sec)                   # number of n steps for fall
-        step_high_n = round((n-step_rise_n-step_fall_n)*dutyMax)    # number of steps for high
-        step_low_n = n-step_rise_n-step_fall_n-step_high_n          # number of steps for low
+        step_rise_n = round(rise/self.sample_sec)                               # number of steps for rise
+        step_fall_n = round(fall/self.sample_sec)                               # number of n steps for fall
+        step_high_n = round((self.period_n-step_rise_n-step_fall_n)*dutyMax)    # number of steps for high
+        step_low_n = self.period_n-step_rise_n-step_fall_n-step_high_n          # number of steps for low
         # define trapezoid wave
-        # {'min'}: iter_min, {'max'}: iter_max, {'grad'}: gradient
+        # {'min'}: iter_min, {'max'}: iter_max, {'grad'}: gradient, {'start'}: start temperatur for this segment
         waveform = {}
         waveform['rise'] = {'min': 0, 'max': step_rise_n-1, 'grad': self.divide(high-low, step_rise_n), 'start': low};                                                                              # rise part
         waveform['high'] = {'min': waveform.get('rise',{}).get('max')+1, 'max': waveform.get('rise',{}).get('max')+1+step_high_n-1, 'grad': 0, 'start': high}                                       # high part
         waveform['fall'] = {'min': waveform.get('high',{}).get('max')+1, 'max': waveform.get('high',{}).get('max')+1+step_fall_n-1, 'grad':  self.divide(low-high, step_fall_n), 'start': high}     # fall part
         waveform['low']  = {'min': waveform.get('fall',{}).get('max')+1, 'max': waveform.get('fall',{}).get('max')+1+step_low_n-1, 'grad': 0, 'start': low}                                         # low part
-        # init?
+        # init iterator
         if ( None != init ):
             if ( math.isnan(init) ): 
                 self.iterator = 0
@@ -190,34 +241,66 @@ class waves:
                 # init wave iterator
                 if ( (True == posSlope) and (0 != step_rise_n) ):       # waveform rises smooth
                     self.iterator = waveform.get('rise',{}).get('min') + round((init-low)/(abs(high-low)/step_rise_n))
-                    return True
                 elif ( (False == posSlope) and (0 != step_fall_n) ):    # waveform falls smooth
                     self.iterator = waveform.get('fall',{}).get('min') + round((high-init)/(abs(high-low)/step_fall_n))
-                    return True
                 else:                                                   # brick wall
                     self.iterator = 0
-                    return True
+        else:
+            if ( 0 != self.verbose ):
+                print("Error: Init function with init arg called")
+            return False
+        # store in common storage element
+        self.trapezoidDict['wave'] = waveform
+        # wave is initialized
+        self.trapezoidDict['isInit'] = True
+        # normal end
+        return True
+    #*****************************
+    
+    
+    #*****************************
+    def trapezoid(self):
+        """
+        @note:              calculates trapezoid waveform
+        
+        @return new:        dictionary with new setting
+        @rtype new:         val: new value; grad: gradient of waveform
+        """
+        # check for init
+        if (True != self.trapezoidDict.get('isInit')):
+            if ( 0 != self.verbose ):
+                print("Error: 'trapezoid' function unintializied, call 'trapezoid_init'")
+            return False
         # calc waveform
-        new = {} 
-        for part in waveform:
+        new = {}
+        for part, wave in self.trapezoidDict.get('wave',{}).items():
             # match part of waveform
-            if ( waveform.get(part,{}).get('min') <= self.iterator <= waveform.get(part,{}).get('max') ):
-                new['val'] = waveform.get(part,{}).get('start') + waveform.get(part,{}).get('grad') * (self.iterator-waveform.get(part,{}).get('min'))
-                new['grad'] = waveform.get(part,{}).get('grad')
+            if ( wave.get('min') <= self.iterator <= wave.get('max') ):
+                new['val'] = wave.get('start') + wave.get('grad') * (self.iterator-wave.get('min'))
+                new['grad'] = wave.get('grad')
         # inc wave iterator, prepare for next calc
         self.iterator += 1
         # jump to start
-        if ( self.iterator > n-1 ):
-            self.iterator -= n
+        if ( self.iterator > self.period_n-1 ):
+            self.iterator -= self.period_n
         # assign to release struct
         return new
     #*****************************    
     
-    
-    
-    
-    
-
-
-
 #------------------------------------------------------------------------------
+    
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    
+    # create object
+    myWave = waves()
+    # check trapezoid function
+    myWave.trapezoid_init(sample=1, period=3600, low=-10, high=30, rise=10, fall=20, init=23, posSlope=True)
+    print(str(myWave.iterator))
+    print(myWave.trapezoid())
+    # check sine function
+    myWave.sine_init(sample=1, period=3600, low=-10, high=30, init=23, posSlope=True)
+    print(str(myWave.iterator))
+    print(myWave.sine())
+#------------------------------------------------------------------------------
+    
