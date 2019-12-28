70c87100295edcebb1527d02bf8430a3ee762fb9
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2019-12-28 18:26:50 +0100

    removed not mandatory try/except loop

--- test/unit/sh641_drv_unittest.py
+++ test/unit/sh641_drv_unittest.py
@@ -28,13 +28,9 @@ import unittest   # performs test
 
 # Module libs
 #
-try:
-    import espec.sh_641_drv as sh_641_drv   # Python Script under test
-    import espec.sh_const as sh_const       # climate chamber defintions
-except:
-    sys.path.append(os.path.abspath((os.path.dirname(os.path.abspath(__file__)) + "/../../")))	# add to default search path, if non IDE execution
-    import espec.sh_641_drv as sh_641_drv   # Python Script under test
-    import espec.sh_const as sh_const       # climate chamber defintions	
+sys.path.append(os.path.abspath((os.path.dirname(os.path.abspath(__file__)) + "/../../")))	# add to default search path, if non IDE execution
+import espec.sh_641_drv as sh_641_drv   # Python Script under test
+import espec.sh_const as sh_const       # climate chamber defintions	
 #------------------------------------------------------------------------------
 
 
